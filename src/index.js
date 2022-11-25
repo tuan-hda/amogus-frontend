3e9399fed1026a8dac149f2de3593c2fcda7c695
Tuấn Akira <anhtuan.hoangdinh@gmail.com>
2022-11-25 10:01:36 +0700

    Add files

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
 
+const darkTheme = createTheme({
+  type: "dark",
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </React.StrictMode>
 );

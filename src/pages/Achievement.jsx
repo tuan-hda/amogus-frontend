f1ef2c813ef0763ee5cbe0d8ca903d58f28a010c
haileyng02 <haileynguyenss@gmail.com>
2022-11-27 06:22:24 +0700

    achievement done

--- src/pages/Achievement.jsx
+++ src/pages/Achievement.jsx
@@ -14,39 +14,11 @@ import { getUserAchievement } from "../api/user";
 import { auth } from "../firebase";
 import { onAuthStateChanged } from "firebase/auth";
 
-const achievements = [
-  {
-    title: "Chuyên gia môi trường",
-    point: "500",
-  },
-  {
-    title: "Bậc thầy môi trường",
-    point: "1000",
-  },
-  {
-    title: "Thạc sĩ môi trường",
-    point: "2000",
-  },
-  {
-    title: "Tiến sĩ môi trường",
-    point: "4000",
-  },
-  {
-    title: "Nhà khoa học môi trường",
-    point: "6000",
-  },
-  {
-    title: "Chúa tể môi trường",
-    point: "10000",
-  },
-];
-
-const point = 10000;
-
 const Achievement = () => {
   const [achievement, setAchievement] = useState({
     achievement: [],
     point: 0,
+    money: 0
   }); 
   const [loading, setLoading] = useState(true);
 
@@ -69,15 +41,36 @@ const Achievement = () => {
     }
   };
 
+  const getAchievementPoint = (achievement) => {
+    switch (achievement) {
+      case "Chuyên gia môi trường":
+        return 500;
+      case "Bậc thầy môi trường":
+        return 1000;
+      case "Thạc sĩ môi trường":
+        return 2000;
+      case "Tiến sĩ môi trường":
+        return 4000;
+      case "Nhà khoa học môi trường":
+        return 6000;
+      case "Chúa tể môi trường":
+        return 10000;
+      default:
+        return 0;
+    }
+  }
+
   useEffect(() => {
     onAuthStateChanged(auth, (userCredential) => {
       if (userCredential) {
         auth.currentUser.getIdToken().then((token) => {
           getUserAchievement(token)
             .then((result) => {
+              console.log(result)
               setAchievement({
                 achievement: result.data.achievement,
                 point: result.data.point,
+                money: result.data.money
               });
             })
             .catch((error) => console.log(error))
@@ -107,7 +100,7 @@ const Achievement = () => {
             <div className="w-[350px] aspect-square flex items-center p-4 mx-auto relative">
               <img src={laurelIcon} alt="Laurel" className="w-full" />
               <p className="text-center text-5xl absolute w-[318px] top-[120px]">
-                {point}
+                {achievement.point}
               </p>
             </div>
           </div>
@@ -117,7 +110,7 @@ const Achievement = () => {
               <h3 className="mb-0">Các chứng nhận đạt được</h3>
             </div>
             <div className="grid grid-flow-col auto-cols-[39%] overflow-x-auto mt-8 gap-x-4 pb-4">
-              {achievements.map((a, i) => (
+              {achievement.achievement.map((a, i) => (
                 <Card
                   key={i}
                   isHoverable
@@ -127,12 +120,12 @@ const Achievement = () => {
                 >
                   <Card.Body>
                     <Card.Image
-                      src={getAchievementPicture(a.title)}
+                      src={getAchievementPicture(a)}
                       width="50%"
                     />
-                    <Text className="text-center ">{a.title}</Text>
+                    <Text className="text-center ">{a}</Text>
                     <Text className="text-center font-bold" color="#07bc0c">
-                      {a.point + "đ"}
+                      {getAchievementPoint(a) + "đ"}
                     </Text>
                   </Card.Body>
                 </Card>
@@ -147,7 +140,7 @@ const Achievement = () => {
             <Row justify="center" css={{ mt: 30 }}>
               <img src={moneyIcon} alt="Money" className="w-10 h-10" />
               <p className="text-2xl ml-4">
-                {(point * 10).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") +
+                {achievement.money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") +
                   "đ"}
               </p>
             </Row>

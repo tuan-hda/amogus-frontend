9804ab831601bce89311edc0e3c20272e8aeb9b4
haileyng02 <haileynguyenss@gmail.com>
2022-11-26 11:03:53 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import AuthLayout from "./layouts/AuthLayout";
+import MainLayout from "./layouts/MainLayout";
 import ForgetPassword from "./pages/ForgetPassword";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
@@ -12,6 +14,9 @@ const App = () => {
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/forgetpassword' element={<ForgetPassword />} />
+      </Route>
+      <Route element={<MainLayout/>}>
+        <Route path="/" element={<Home/>}/>
       </Route>
     </Routes>
   );

071644252e4b008de5966eda12b0f1b32885845f
haileyng02 <haileynguyenss@gmail.com>
2022-11-26 22:55:40 +0700

    achievements

---
+++ src/assets/achievements/index.js
@@ -0,0 +1,7 @@
+import bacThayIcon from './bac_thay.png';
+import chuaTeIcon from './chua_te.png';
+import chuyenGiaIcon from './chuyen_gia.png';
+import khoaHocIcon from './khoa_hoc.png';
+import thacSiIcon from './thac_si.png';
+import tienSiIcon from './tien_si.png';
+export { bacThayIcon, chuaTeIcon, chuyenGiaIcon, khoaHocIcon, thacSiIcon, tienSiIcon };
